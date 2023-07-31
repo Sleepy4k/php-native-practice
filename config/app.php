75b9a0c9498c50a813de83dfe0ad2694215afc1e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-31 18:52:09 +0700

    feat: app config

---
+++ config/app.php
@@ -0,0 +1,20 @@
+<?php
+
+$GLOBALS["APP_NAME"] = [
+  "app" => [
+    "name" => "My App",
+    "version" => "1.0.0",
+  ],
+  "database" => [
+    "hostname" => "localhost",
+    "port" => 3306,
+    "username" => "root",
+    "password" => "",
+    "name" => "db_native",
+  ],
+  "password" => [
+    "algo" => PASSWORD_BCRYPT,
+    "cost" => 10,
+    "salt" => 50
+  ]
+];
