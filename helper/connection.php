3af4dbb7c058502be79e02b7b91f803934420dc3
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-31 18:52:47 +0700

    chore: connection helper

--- helper/connection.php
+++ helper/connection.php
@@ -1,5 +1,12 @@
 <?php
-  $conn = mysqli_connect("localhost", "root", "", "db_native");
+  include_once '../core/config.php';
+
+  $DB_HOST = Config::get('database/hostname');
+  $DB_USER = Config::get('database/username');
+  $DB_PASS = Config::get('database/password');
+  $DB_NAME = Config::get('database/name');
+
+  $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
   if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
