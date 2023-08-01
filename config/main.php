c8b91d6b5b9f4286933ba91528683bce28008a4c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-08-02 01:26:10 +0700

    chore: main config

--- config/main.php
+++ config/main.php
@@ -1,16 +1,20 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-$GLOBALS["APP_NAME"] = [
+$dotenv = include_once BASEPATH . './core/DotEnv.php';
+$env = new DotEnv(BASEPATH);
+
+$GLOBALS["APP_CONFIG"] = [
   "app" => [
-    "name" => "Native PHP Framework",
-    "version" => "1.0.0",
+    "name" => $env->get("APP_NAME", "Native PHP"),
+    "version" => $env->get("APP_VERSION", "1.0.0"),
   ],
   "database" => [
-    "hostname" => "localhost",
-    "port" => 3306,
-    "username" => "root",
-    "password" => "",
-    "name" => "db_native",
+    "hostname" => $env->get("DB_HOST", "localhost"),
+    "port" => $env->get("DB_PORT", 3306),
+    "username" => $env->get("DB_USERNAME", "root"),
+    "password" => $env->get("DB_PASSWORD", ""),
+    "name" => $env->get("DB_NAME", "db_native"),
   ],
   "password" => [
     "algo" => PASSWORD_BCRYPT,
