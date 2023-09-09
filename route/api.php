fef9919c130322f7ddf4d98efb0b2029b48170a1
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-09 18:57:11 +0700

    chore: all route

--- route/api.php
+++ route/api.php
@@ -1,26 +1,6 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-include_once BASEPATH . './core/Routes.php';
-include_once BASEPATH . './model/UserModel.php';
+use Snake\Core\Routing\Route;
+use Bin\Controllers\Api\WelcomeController;
 
-$route = new Route("api");
-
-$route->get('/users', function() {
-  if (!isset($_SESSION['user_api'])) {
-    echo json_encode([
-      'status' => false,
-      'message' => 'Unauthorized'
-    ]);
-    exit;
-  }
-
-  $model = new UserModel();
-  $query_user = $model->get();
-
-  echo json_encode([
-    'status' => true,
-    'message' => 'Success',
-    'data' => $query_user
-  ]);
-});
+Route::any('/', WelcomeController::class);
