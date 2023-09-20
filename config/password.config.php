2946b8dfc93c594c189f6ad79aa14a3712dfb0e2
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-20 12:10:25 +0700

    chore: some config

--- config/password.config.php
+++ config/password.config.php
@@ -1,7 +1,35 @@
 <?php
 
 return [
-  'algo' => env('PASSWORD_ALGO', PASSWORD_BCRYPT),
-  'cost' => env('PASSWORD_COST', 10),
-  'salt' => env('PASSWORD_SALT', 50)
+  /*
+  |--------------------------------------------------------------------------
+  | Password Algorithm
+  |--------------------------------------------------------------------------
+  |
+  | The algorithm to use for hashing passwords.
+  |
+  | Supported: PASSWORD_BCRYPT, PASSWORD_ARGON2I, PASSWORD_ARGON2ID
+  |
+  */
+  'algorithm' => PASSWORD_BCRYPT,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Password Cost
+  |--------------------------------------------------------------------------
+  |
+  | The algorithmic cost that should be used.
+  |
+  */
+  'cost' => 10,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Password Salt
+  |--------------------------------------------------------------------------
+  |
+  | The salt to use for hashing passwords.
+  |
+  */
+  'salt' => 50,
 ];
